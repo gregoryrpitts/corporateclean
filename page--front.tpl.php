5b4246e50196e9bbe53f7a2724e236f9bda84d66
Snatch <gregory.r.pitts@gmail.com>
2015-06-07 11:04:48 -0700

    Added front page template PHP file

---
+++ page--front.tpl.php
@@ -0,0 +1,129 @@
+<!-- #header -->
+<div id="header">
+	<!-- #header-inside -->
+    <div id="header-inside" class="container_12 clearfix">
+    	<!-- #header-inside-left -->
+        <div id="header-inside-left" class="grid_8">
+            
+            <?php if ($logo): ?>
+            <a href="<?php print check_url($front_page); ?>" title="<?php print t('Home'); ?>"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+            <?php endif; ?>
+     
+            <?php if ($site_name || $site_slogan): ?>
+            <div class="clearfix">
+            <?php if ($site_name): ?>
+            <span id="site-name"><a href="<?php print check_url($front_page); ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a></span>
+            <?php endif; ?>
+            <?php if ($site_slogan): ?>
+            <span id="slogan"><?php print $site_slogan; ?></span>
+            <?php endif; ?>
+            </div>
+            <?php endif; ?>
+            
+        </div><!-- EOF: #header-inside-left -->
+        
+        <!-- #header-inside-right -->    
+        <div id="header-inside-right" class="grid_4">
+
+			<?php print render($page['search_area']); ?>
+
+        </div><!-- EOF: #header-inside-right -->
+    
+    </div><!-- EOF: #header-inside -->
+
+</div><!-- EOF: #header -->
+
+
+
+
+<!-- #content -->
+<div id="content">
+	<!-- #content-inside -->
+    <div id="content-inside" class="container_12 clearfix">
+        
+            <div id="main" class="grid_12">    
+        
+            <?php if (theme_get_setting('breadcrumb_display','corporateclean')): print $breadcrumb; endif; ?>
+            
+            <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+       
+            <?php if ($messages): ?>
+            <div id="console" class="clearfix">
+            <?php print $messages; ?>
+            </div>
+            <?php endif; ?>
+     
+            <?php if ($page['help']): ?>
+            <div id="help">
+            <?php print render($page['help']); ?>
+            </div>
+            <?php endif; ?>
+            
+            <?php if ($action_links): ?>
+            <ul class="action-links">
+            <?php print render($action_links); ?>
+            </ul>
+            <?php endif; ?>
+            
+			<?php print render($title_prefix); ?>
+            <?php if ($title): ?>
+            <h1><?php print $title ?></h1>
+            <?php endif; ?>
+            <?php print render($title_suffix); ?>
+            
+            <?php if ($tabs): ?><?php print render($tabs); ?><?php endif; ?>
+            
+            <?php print render($page['content']); ?>
+            
+            <?php print $feed_icons; ?>
+            
+        </div><!-- EOF: #main -->
+
+    </div><!-- EOF: #content-inside -->
+
+</div><!-- EOF: #content -->
+
+<!-- #footer -->    
+<div id="footer">
+	<!-- #footer-inside -->
+    <div id="footer-inside" class="container_12 clearfix">
+    
+        <div class="footer-area grid_4">
+        <?php print render($page['footer_first']); ?>
+        </div><!-- EOF: .footer-area -->
+        
+        <div class="footer-area grid_4">
+        <?php print render($page['footer_second']); ?>
+        </div><!-- EOF: .footer-area -->
+        
+        <div class="footer-area grid_4">
+        <?php print render($page['footer_third']); ?>
+        </div><!-- EOF: .footer-area -->
+       
+    </div><!-- EOF: #footer-inside -->
+
+</div><!-- EOF: #footer -->
+
+<!-- #footer-bottom -->    
+<div id="footer-bottom">
+
+	<!-- #footer-bottom-inside --> 
+    <div id="footer-bottom-inside" class="container_12 clearfix">
+    	<!-- #footer-bottom-left --> 
+    	<div id="footer-bottom-left" class="grid_8">
+        
+            <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('class' => array('secondary-menu', 'links', 'clearfix')))); ?>
+            
+            <?php print render($page['footer']); ?>
+            
+        </div>
+    	<!-- #footer-bottom-right --> 
+        <div id="footer-bottom-right" class="grid_4">
+        
+        	<?php print render($page['footer_bottom_right']); ?>
+        
+        </div><!-- EOF: #footer-bottom-right -->
+       
+    </div><!-- EOF: #footer-bottom-inside -->
+
+</div><!-- EOF: #footer -->
